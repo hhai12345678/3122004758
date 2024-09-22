b7e6e71f11b34e8c959ec8fc7e89829e07091eae
25406 <2540615427@qq.com>
2024-09-22 16:23:05 +0800

    Default Changelist

--- main.py
+++ main.py
@@ -30,3 +30,49 @@ def generate_expression(max_range, max_operators):
     return expression
 
 
+def calculate_expression(expression):
+    try:
+        return eval(expression, {"__builtins__": None}, {})
+    except ZeroDivisionError:
+        return None
+
+
+def generate_exercise(num_exercises, max_range, max_operators):
+    exercises = []
+    answers = []
+    while len(exercises) < num_exercises:
+        expression = generate_expression(max_range, max_operators)
+        result = calculate_expression(expression)
+        if result is not None:
+            exercises.append(expression)
+            answers.append(str(result))
+    return exercises, answers
+
+
+def write_to_file(filename, lines):
+    with open(filename, 'w', encoding='utf-8') as file:
+        for line in lines:
+            file.write(line + '\n')
+
+
+def read_file(filename):
+    with open(filename, 'r', encoding='utf-8') as file:
+        return [line.strip() for line in file]
+
+
+def check_answers(exercise_file, answer_file):
+    exercises = read_file(exercise_file)
+    answers = read_file(answer_file)
+    correct_indices = []
+    wrong_indices = []
+
+    for index, (exercise, user_answer) in enumerate(zip(exercises, answers), start=1):
+        correct_answer = str(calculate_expression(exercise))
+        if user_answer == correct_answer:
+            correct_indices.append(index)
+        else:
+            wrong_indices.append(index)
+
+    return correct_indices, wrong_indices
+
+
