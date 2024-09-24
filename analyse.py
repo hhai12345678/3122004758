769848440eae2c875808455743e3a338e0cc5bfb
25406 <2540615427@qq.com>
2024-09-24 21:13:31 +0800

    Default Changelist

--- analyse.py
+++ analyse.py
@@ -1,1 +1,23 @@
+import cProfile
+import pstats
+import io
+from main import main  # 假设你的主函数名为main
+
+def profile_code():
+    pr = cProfile.Profile()
+    pr.enable()
+
+    main()  # 这里运行你的主程序
+
+    pr.disable()
+    s = io.StringIO()
+    ps = pstats.Stats(pr, stream=s).sort_stats(pstats.SortKey.TIME)
+    ps.print_stats()
+
+    with open('profile_results.txt', 'w') as f:
+        f.write(s.getvalue())
+
+if __name__ == "__main__":
+    profile_code()
+
 
